9f3fff0d295b56d13a597046b13b570eeeb98b6d
icorvus <business.wrona@gmail.com>
2022-05-24 12:55:57 +0200

    Added comments

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import string
 
 
 def caesar_cipher(secret, shift_factor):
-    shift_factor %= 26
+    shift_factor %= 26 # There are 26 letters in latin alphabeth
     output_string = ""
     for char in secret:
         if char in string.ascii_lowercase:
             char_unicode = ord(char) + shift_factor
-            if char_unicode > 122:
+            if char_unicode > 122: # 122 is unicode for "z"
                 char_unicode -= 26
             char = chr(char_unicode)
 
         if char in string.ascii_uppercase:
             char_unicode = ord(char) + shift_factor
-            if char_unicode > 90:
+            if char_unicode > 90: # 90 is unicode for "Z"
                 char_unicode -= 26
             char = chr(char_unicode)
             
